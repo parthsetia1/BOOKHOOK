d041b8f91b3cbc3f08b93983db65bebc01054b7f
Parth Setia <Parth.Setia@airtel.com>
2025-12-29 00:08:06 +0530

    fix

--- utils/supabase.py
+++ utils/supabase.py
@@ -1,7 +1,11 @@
-from supabase import create_client
 import os
+from supabase_py import create_client
 
 def get_supabase():
     url = os.getenv("SUPABASE_URL")
     key = os.getenv("SUPABASE_KEY")
+
+    if not url or not key:
+        raise Exception("Missing Supabase env vars")
+
     return create_client(url, key)
